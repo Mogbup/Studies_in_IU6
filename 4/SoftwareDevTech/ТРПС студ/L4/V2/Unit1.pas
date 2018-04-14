a9f2c43e8e4e6993b792ffb2dda14cf22e784f26
Roman Vekshin <v.r.d.1811@gmail.com>
2018-04-14 12:36:09 +0300

    Добавление задания в формате PDF

--- 4/SoftwareDevTech/ТРПС студ/L4/V2/Unit1.pas
+++ 4/SoftwareDevTech/ТРПС студ/L4/V2/Unit1.pas
@@ -16,62 +16,90 @@ type
   public
     { Public declarations }
   end;
-Mas=array [1..2,1..100] of real;
+
+  Mas = array [1..2, 1..100] of real;
 
 var
   Form1: TForm1;
-  m1,m2:Mas;
-  i,Mx,My,Xs,Ys:integer;
-   x,dx:real;
-  implementation
+  m1, m2: Mas;
+  i, Mx, My, Xs, Ys: integer;
+  x, dx: real;
+
+implementation
+
 {$R *.dfm}
-Function y1(x:real):real; begin   y1:=abs(cos(x)*x) end;
-Function y2(x:real):real; begin   y2:=abs(sin(x)) end;
+
+function y1(x: real): real;
+begin
+  y1 := abs(cos(x) * x);
+end;
+
+function y2(x: real): real;
+begin
+  y2 := abs(sin(x));
+end;
+
 procedure TForm1.Button1Click(Sender: TObject);
 begin
-With Image1.Canvas do
-   begin    pen.width:=2; Pen.Color:=ClBlue;
-            rectangle(1,1,Image1.Width,Image1.height);
-   end;
-dx:=(4*pi)/100;
-x:=-2*pi;
-For i:=1 to 100 do
-begin m1[1,i]:=x; m1[2,i]:=y1(x); x:=x+dx; end;
-x:=-2*pi;
-For i:=1 to 100 do
-begin m2[1,i]:=x; m2[2,i]:=y2(x); x:=x+dx; end;
-
-Image1.Canvas.Pen.width:=5;
- {масштаб и смещение}
-Mx:=105; My:=50; Xs:=0; Ys:=Image1.Height div 2;
-
-  {рисуем 1-ю функцию}
-Image1.Canvas.Pen.Color:=ClRed;
-Image1.Canvas.MoveTo(round(m1[1,1]*Mx+Xs),
-                     round(-m1[2,1]*My+Image1.Height-Ys) );
-for i:=2 to 100 do begin
-Image1.Canvas.LineTo(round(m1[1,i]*Mx+Xs),
-                     round(-m1[2,i]*My+Image1.Height-Ys) );
+  with Image1.Canvas do
+  begin
+    pen.Width := 2;
+    Pen.Color := ClBlue;
+    rectangle(1, 1, Image1.Width, Image1.Height);
   end;
-  {рисуем 2-ю функцию}
-Image1.Canvas.Pen.Color:=ClGreen;
-Image1.Canvas.MoveTo(round(m2[1,1]*Mx+Xs),
-                     round(-m2[2,1]*My+Image1.Height-Ys) );
-for i:=2 to 100 do begin
-Image1.Canvas.LineTo(round(m2[1,i]*Mx+Xs),
-                     round(-m2[2,i]*My+Image1.Height-Ys) );
+  dx := (4 * pi) / 100;
+  x := -2 * pi;
+  for i := 1 to 100 do
+  begin
+    m1[1, i] := x;
+    m1[2, i] := y1(x);
+    x := x + dx;
+  end;
+  x := -2 * pi;
+  for i := 1 to 100 do
+  begin
+    m2[1, i] := x;
+    m2[2, i] := y2(x);
+    x := x + dx;
   end;
 
-  {Оси X и У}
-With Image1.Canvas do  begin
-Pen.width:=2; Pen.Color:=ClBlue;
-MoveTo(0,Image1.Height div 2);
-LineTo(Image1.Width,Image1.Height div 2);
-MoveTo(Image1.Width div 2,0);
-LineTo(Image1.Width div 2,Image1.Height);
-end;
+  Image1.Canvas.Pen.Width := 5;
+  {РјР°СЃС€С‚Р°Р± Рё СЃРјРµС‰РµРЅРёРµ}
+  Mx := 105;
+  My := 50;
+  Xs := 0;
+  Ys := Image1.Height div 2;
+
+  {СЂРёСЃСѓРµРј 1-СЋ С„СѓРЅРєС†РёСЋ}
+  Image1.Canvas.Pen.Color := ClRed;
+  Image1.Canvas.MoveTo(round(m1[1, 1] * Mx + Xs),
+    round(-m1[2, 1] * My + Image1.Height - Ys));
+  for i := 2 to 100 do
+  begin
+    Image1.Canvas.LineTo(round(m1[1, i] * Mx + Xs),
+      round(-m1[2, i] * My + Image1.Height - Ys));
+  end;
+  {СЂРёСЃСѓРµРј 2-СЋ С„СѓРЅРєС†РёСЋ}
+  Image1.Canvas.Pen.Color := ClGreen;
+  Image1.Canvas.MoveTo(round(m2[1, 1] * Mx + Xs),
+    round(-m2[2, 1] * My + Image1.Height - Ys));
+  for i := 2 to 100 do
+  begin
+    Image1.Canvas.LineTo(round(m2[1, i] * Mx + Xs),
+      round(-m2[2, i] * My + Image1.Height - Ys));
+  end;
+
+  {РћСЃРё X Рё РЈ}
+  with Image1.Canvas do
+  begin
+    Pen.Width := 2;
+    Pen.Color := ClBlue;
+    MoveTo(0, Image1.Height div 2);
+    LineTo(Image1.Width, Image1.Height div 2);
+    MoveTo(Image1.Width div 2, 0);
+    LineTo(Image1.Width div 2, Image1.Height);
+  end;
 end;
 
 
 end.
-
